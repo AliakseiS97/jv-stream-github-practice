c8669bb78b859a79a5c3966262ae3fcad58fcd65
AliakseiS97 <aliakseis97@gmail.com>
2025-10-17 17:15:15 +0200

    First solution1.

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -104,7 +104,7 @@ public class StreamPractice {
      * names sorted alphabetically.
      * The requirements are: person should be older than 35 years, should be allowed to vote,
      * have nationality - 'Ukrainian'
-     * and live in Ukraine for 10 years. For the last requirement use field periodsInUkr,
+     * and live in Ukraine for 10 years.For the last requirement use field periodsInUkr,
      * which has following view: "2002-2015"
      * We want to reuse our validation in future, so let's write our own impl of Predicate
      * parametrized with Candidate in CandidateValidator.
